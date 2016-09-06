bd62dbded9e4981f4ef7d1029933190e49f464ae
Tim Moore <tcmoore3@gmail.com>
2016-09-06 11:54:47 -0500

    Fix try/except in legend()

--- timplotlib/timplotlib.py
+++ timplotlib/timplotlib.py
@@ -9,10 +9,10 @@ def lighter_ticks(ax, color='#D3D3D3'):
 def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1):
     try:
         legend = ax.legend(loc=loc, fancybox=fancybox)
+        legend.legendPatch.set_edgecolor(ec)
+        legend.legendPatch.set_lw(lw)
     except AttributeError:
         return 
-    legend.legendPatch.set_edgecolor(ec)
-    legend.legendPatch.set_lw(lw)
 
 def timize(ax, framecolor='#D3D3D3', tickcolor='#D3D3D3', legend=True, legend_loc=0,
         fancybox=True, legend_ec='#D3D3D3', legend_lw=0.1, grid=True):
