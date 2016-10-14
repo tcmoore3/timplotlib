2dab13a29c5552d346d6185d4480750fa02d3381
Tim Moore <tcmoore3@gmail.com>
2016-10-14 11:15:04 -0500

    Make axes spines drawn last by default
    I was previously drawing the grid after the plot was initialized, but
    since the axes frame (i.e., spine) is lighter than the grid lines, the
    grid lines that are at the axis limits were being drawn on top of the
    spines which was ugly.
    This fixes that by telling it to draw the axes spines last (read: over
    the grid lines) by default.

--- timplotlib/timplotlib.py
+++ timplotlib/timplotlib.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-def lighter_frame(ax, color='#D3D3D3'):
-    plt.setp(ax.spines.values(), color=color)
+def lighter_frame(ax, color='#D3D3D3', zorder='top'):
+    plt.setp(ax.spines.values(), color=color, zorder=zorder)
 
 def lighter_ticks(ax, color='#D3D3D3'):
-    plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='#D3D3D3')
+    plt.setp([ax.get_xticklines(), ax.get_yticklines()], color=color)
 
 def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1):
     try:
@@ -15,8 +15,8 @@ def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1):
         return 
 
 def timize(ax, framecolor='#D3D3D3', tickcolor='#D3D3D3', legend=True, legend_loc=0,
-        fancybox=True, legend_ec='#D3D3D3', legend_lw=0.1, grid=True):
-    lighter_frame(ax, color=framecolor)
+        fancybox=True, legend_ec='#D3D3D3', legend_lw=0.1, grid=True, frame_z='top'):
+    lighter_frame(ax, color=framecolor, zorder=frame_z)
     lighter_ticks(ax, color=tickcolor)
     if legend == True:
         fancy_legend(ax, loc=legend_loc, fancybox=fancybox, ec=legend_ec, lw=legend_lw)
