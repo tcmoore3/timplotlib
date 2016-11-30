0813d514791b2f6aef16e0b9904c099f311803f5
Tim Moore <tcmoore3@gmail.com>
2016-11-29 22:38:39 -0600

    Fix legend_cspacing error

--- timplotlib/timplotlib.py
+++ timplotlib/timplotlib.py
@@ -6,9 +6,11 @@ def lighter_frame(ax, color='#D3D3D3', zorder='top'):
 def lighter_ticks(ax, color='#D3D3D3'):
     plt.setp([ax.get_xticklines(), ax.get_yticklines()], color=color)
 
-def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1, ncol=1):
+def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1, ncol=1,
+        columnspacing=None):
     try:
-        legend = ax.legend(loc=loc, fancybox=fancybox, ncol=ncol)
+        legend = ax.legend(loc=loc, fancybox=fancybox, ncol=ncol, 
+                columnspacing=columnspacing)
         legend.legendPatch.set_edgecolor(ec)
         legend.legendPatch.set_lw(lw)
     except AttributeError:
@@ -16,7 +18,7 @@ def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1, ncol=1):
 
 def timize(ax, framecolor='#D3D3D3', tickcolor='#D3D3D3', legend=True, legend_loc=0,
         fancybox=True, legend_ec='#D3D3D3', legend_lw=0.1, grid=True, frame_z='top',
-        grid_zorder=0, legend_ncol=1, grid_color='#D3D3D3'):
+        grid_zorder=0, legend_ncol=1, grid_color='#D3D3D3', legend_cspacing=None):
     lighter_frame(ax, color=framecolor, zorder=frame_z)
     lighter_ticks(ax, color=tickcolor)
     if legend == True:
