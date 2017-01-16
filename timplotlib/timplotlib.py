a6e1c3d41cbd137ea4943f2b263255d91b302215
Tim Moore <tcmoore3@gmail.com>
2017-01-16 11:46:46 -0600

    Change method of updating frame/tick colors
    Instead of using plt.setp(), do this explicitly for each axes object.

--- timplotlib/timplotlib.py
+++ timplotlib/timplotlib.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
 def lighter_frame(ax, color='#D3D3D3', zorder='top'):
-    plt.setp(ax.spines.values(), color=color, zorder=zorder)
+    for spine in ('top', 'bottom', 'left', 'right'):
+        ax.spines[spine].set_color(color)
 
 def lighter_ticks(ax, color='#D3D3D3'):
-    plt.setp([ax.get_xticklines(), ax.get_yticklines()], color=color)
+    for t in ax.xaxis.get_ticklines():
+        t.set_color(color)
+    for t in ax.yaxis.get_ticklines():
+        t.set_color(color)
 
 def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1, ncol=1,
         columnspacing=None):
@@ -19,7 +23,7 @@ def fancy_legend(ax, loc=0, fancybox=True, ec='#D3D3D3', lw=0.1, ncol=1,
 def timize(ax, framecolor='#D3D3D3', tickcolor='#D3D3D3', legend=True, legend_loc=0,
         fancybox=True, legend_ec='#D3D3D3', legend_lw=0.1, grid=True, frame_z='top',
         grid_zorder=0, legend_ncol=1, grid_color='#D3D3D3', legend_cspacing=None):
-    lighter_frame(ax, color=framecolor, zorder=frame_z)
+    lighter_frame(ax, color=framecolor)
     lighter_ticks(ax, color=tickcolor)
     if legend == True:
         fancy_legend(ax, loc=legend_loc, fancybox=fancybox, ec=legend_ec, lw=legend_lw,
